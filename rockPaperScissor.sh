b16ff62e5e92426fc3949f75f2a0f91703e0d26d
biswajitdixit <buntyoec1998@gmail.com>
2021-01-11 10:39:58 +0530

    UPDATED: rps simulation

---
+++ rockPaperScissor.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+
+comguess=$(( ($RANDOM % 3) + 1 ))
+rps=(no rock paper scissors)
+
+declare -a rps;
+comguess=$(( ($RANDOM % 3) + 1 ))
+echo "The computer chose ${rps[$comguess]}"
+
+echo -n "Please choose (1 = rock / 2 = paper / 3 = scissors): "
+read choice
+
+
+   echo ""
+   if [ $choice = $comguess ] ;
+ then
+    echo " TIED!"
+
+   elif [ $choice -eq 1 -a $comguess -eq 3 ]
+ then
+     echo " your rock won "
+     wins=$(( $wins + 1 ))
+   elif [ $choice -eq 2 -a $comguess -eq 1 ]
+ then
+     echo "your paper won"
+     wins=$(( $wins + 1 ))
+   elif [ $choice -eq 3 -a $comguess -eq 2 ]
+then
+     echo -n "Your scissors cut - and beat - the computer paper "
+     wins=$(( $wins + 1 ))
+   elif [ $choice -eq 3 -a $comguess -eq 1 ]
+ then
+     echo " computer rock won"
+
+   elif [ $choice -eq 1 -a $comguess -eq 2 ]
+ then
+     echo " computer paper won "
+
+   elif [ $choice -eq 2 -a $comguess-eq 3 ]
+ then
+     echo -n "The computer's scissors cut - and beat your paper "
+   else
+     echo " choice=$choice and comguess=$computer"
+   fi
